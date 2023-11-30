79b9f2fd270badbe3b59b8417f1316d9c44b8e97
Maite Canaviri <maite.c.luque@gmail.com>
2023-11-30 00:13:08 -0300

    Fix like post request handler

--- src/middlewares/checkValidationResult.js
+++ src/middlewares/checkValidationResult.js
@@ -1,15 +1,15 @@
-const { validationResult } = require("express-validator")
+const { validationResult } = require("express-validator");
 
 function checkValidationResult(req, res, next) {
-  const result = validationResult(req)
+  const result = validationResult(req);
   if (result.isEmpty()) {
-    return next()
+    return next();
   }
   return res.status(400).json({
     status: "fail",
     message: "The parameters are not valid",
     errors: result.array(),
-  })
+  });
 }
 
-export default checkValidationResult
+module.exports = checkValidationResult;
--- src/routes/like.js
+++ src/routes/like.js
@@ -1,11 +1,11 @@
-const { query, body } = require("express-validator")
-const Like = require("../models/like")
-const checkValidationResult = require("../middlewares/checkValidationResult")
-const Recipe = require("../models/recipe")
-const User = require("../models/user")
-const mongoose = require("mongoose")
+const { query, body } = require("express-validator");
+const Like = require("../models/like");
+const checkValidationResult = require("../middlewares/checkValidationResult");
+const Recipe = require("../models/recipe");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
-const router = require("express").Router()
+const router = require("express").Router();
 
 router.get(
   "/",
@@ -13,115 +13,118 @@ router.get(
   checkValidationResult,
   async (req, res) => {
     try {
-      const results = await Like.find(req.query)
+      const results = await Like.find(req.query);
       return res.status(200).json({
         status: "success",
         data: results,
-      })
+      });
     } catch (error) {
       return res.status(500).json({
         status: "error",
         message: error.message,
-      })
+      });
     }
   }
-)
+);
 
 router.post(
   "/",
   body(["authorUser", "targetRecipe"]).isMongoId(),
   checkValidationResult,
   async (req, res) => {
-    const session = await mongoose.startSession()
-    session.startTransaction()
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
     try {
       const existsLike = await Like.exists({
         authorUser: req.body.authorUser,
         targetRecipe: req.body.targetRecipe,
-      })
+      });
+
       if (existsLike) {
         return res.status(400).json({
           status: "fail",
           message: "The operation is not valid",
-        })
+        });
       }
 
-      const existsUser = await User.findById(req.body.authorUser)
+      const existsUser = await User.findById(req.body.authorUser);
       if (!existsUser) {
         return res.status(400).json({
           status: "fail",
           message: "The user does not exists",
-        })
+        });
       }
 
-      const recipe = await Recipe.findById(req.body.targetRecipe)
+      const recipe = await Recipe.findById(req.body.targetRecipe);
       if (!recipe) {
         return res.status(400).json({
           status: "fail",
           message: "The recipe does not exists",
-        })
+        });
       }
 
       const newLike = new Like({
         authorUser: req.body.authorUser,
         targetRecipe: req.body.targetRecipe,
-      })
-      recipe.$inc({ likes: 1 })
-      await recipe.save()
-      await newLike.save()
+      });
+      recipe.$inc("likes", 1);
+      await recipe.save();
+      await newLike.save();
 
-      await session.commitTransaction()
-      session.endSession()
+      await session.commitTransaction();
+      session.endSession();
 
-      return res.status(204).send()
+      return res.status(204).send();
     } catch (error) {
-      await session.abortTransaction()
-      session.endSession()
+      await session.abortTransaction();
+      session.endSession();
 
       return res.status(500).json({
         status: "error",
         message: error.message,
-      })
+      });
     }
   }
-)
+);
 
 router.delete(
   "/",
   body(["authorUser", "targetRecipe"]).isMongoId(),
   checkValidationResult,
   async (req, res) => {
-    const session = await mongoose.startSession()
-    session.startTransaction()
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
     try {
       const result = await Like.findOneAndDelete({
         authorUser: req.body.authorUser,
         targetRecipe: req.body.targetRecipe,
-      })
+      });
       if (!result) {
         return res.status(400).json({
           status: "fail",
           message: "Invalid operation",
-        })
+        });
       }
-      const recipe = await Recipe.findById(req.body.targetRecipe)
-      recipe.$inc({ likes: -1 })
-      await recipe.save()
+      const recipe = await Recipe.findById(req.body.targetRecipe);
+      recipe.$inc("likes", -1);
+      await recipe.save();
 
-      await session.commitTransaction()
-      session.endSession()
+      await session.commitTransaction();
+      session.endSession();
 
-      return res.status(204).send()
+      return res.status(204).send();
     } catch (error) {
-      await session.abortTransaction()
-      session.endSession()
+      await session.abortTransaction();
+      session.endSession();
 
       return res.status(500).json({
         status: "error",
         message: error.message,
-      })
+      });
     }
   }
-)
+);
+
+module.exports = router;
