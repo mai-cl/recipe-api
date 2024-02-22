5188295efd532b0a259b577e3c8d70fd3fe582d9
Maite Canaviri <maite.c.luque@gmail.com>
2024-02-21 21:35:45 -0300

    Remove POST /users request handler

--- src/routes/user.js
+++ src/routes/user.js
@@ -59,57 +59,6 @@ router.get(
   }
 );
 
-router.post(
-  "/",
-  [
-    body("username").notEmpty().isLength({ min: 2, max: 16 }),
-    body("email").notEmpty().isEmail(),
-    body(["password", "passwordConfirm"])
-      .isStrongPassword({
-        minLength: 8,
-        minNumbers: 1,
-        minSymbols: 0,
-        minUppercase: 1,
-        minLowercase: 1,
-      })
-      .trim(),
-    body("role").optional().isString(),
-    body("photo").optional().isDataURI(),
-  ],
-  checkValidationResult,
-  checkIfPasswordsAreEqual,
-  async (req, res) => {
-    try {
-      const encryptedPassword = await bcrypt.hash(req.body.password, 10);
-
-      const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: encryptedPassword,
-        role: req.body.role || "USER",
-        photo: null,
-      });
-
-      if (req.body.photo) {
-        urlImg = await uploadProfilePhoto(req.body.photo);
-        newUser.photo = urlImg;
-      }
-
-      const result = await newUser.save();
-
-      return res.status(201).json({
-        status: "success",
-        data: result,
-      });
-    } catch (e) {
-      return res.status(500).json({
-        status: "error",
-        message: e.message,
-      });
-    }
-  }
-);
-
 router.delete(
   "/:id",
   protect,
